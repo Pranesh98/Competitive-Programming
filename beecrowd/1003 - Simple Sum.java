4e2c469b265d22dc65a79ce2487904c3e97ed9f6
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-02-08 22:28:05 +0600

    Create 1003 - Simple Sum.java

---
+++ beecrowd/1003 - Simple Sum.java
@@ -0,0 +1,18 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        int A, B;
+        
+        // Input A, B.
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.printf ("SOMA = %d\n",A+B);
+    }
+        
+ 
+}
