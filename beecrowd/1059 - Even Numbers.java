6c2aee38d4d2adf3abbd6666e483e5171b7b440a
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-02-08 22:36:17 +0600

    Create 1059 - Even Numbers.java

---
+++ beecrowd/1059 - Even Numbers.java
@@ -0,0 +1,19 @@
+/*
+    Programmer : "Pranesh Chowdhury".
+    Link: https://www.beecrowd.com.br/judge/en/problems/view/1059
+*/
+
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String []args){
+        
+        for (int a = 1; a<=100;a++){
+            if (a%2==0){
+                System.out.printf ("%d\n",a);
+            }
+        }
+      
+    }
+}
