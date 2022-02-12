2e19511b3b548c91a898f03544c0dc37e13aa0f4
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-02-13 02:53:18 +0600

    C++

---
+++ Codeforces/4A - Watermelon.cpp
@@ -0,0 +1,17 @@
+// Link: https://codeforces.com/problemset/problem/4/A
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;     // w = weight.
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2){
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
